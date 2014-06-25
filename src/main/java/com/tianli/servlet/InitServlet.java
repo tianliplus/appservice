53f94a05f93a9d13bd9ea78fadf0947d92827faa
Tian Li <tianliplus@gmail.com>
2014-06-25 22:03:19 +0800

    add init method

---
+++ src/main/java/com/tianli/servlet/InitServlet.java
@@ -0,0 +1,47 @@
+package com.tianli.servlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.google.gson.Gson;
+import com.tianli.result.BaseResult;
+
+/**
+ * Servlet implementation class InitServlet
+ */
+public class InitServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public InitServlet() {
+        super();
+    }
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		BaseResult result = new BaseResult();
+		String userName = request.getParameter("username");
+		String hostIp = request.getParameter("hostip");
+		if (userName != null) {
+			result.rcode = 1;
+		}
+		Gson gson = new Gson();
+		PrintWriter out = response.getWriter();
+		out.println(gson.toJson(result));
+		return;
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+	}
+
+}
