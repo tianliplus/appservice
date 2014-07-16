d8ae142b6bd2bd2483d09986e44f64ed667eaf0a
Tian Li <tianliplus@gmail.com>
2014-07-16 14:57:15 +0800

    add doPost

--- src/main/java/com/tianli/servlet/TextServlet.java
+++ src/main/java/com/tianli/servlet/TextServlet.java
@@ -52,7 +52,7 @@ public class TextServlet extends HttpServlet {
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-
+		doGet(request, response);
 	}
 
 }
